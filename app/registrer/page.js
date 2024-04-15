9a8f5ea7ec0036b0836a4ce375f096c96d33498b
Yusuf <yusuf.salad.yusuf@gmail.com>
2024-04-16 00:23:28 +0200

    endringer i navbar.js, registrer/page.js

--- app/registrer/page.js
+++ app/registrer/page.js
@@ -67,25 +67,25 @@ export default function Registrer() {
     </div>
     <div className="inputbox">
     <label><strong>Etternavn</strong></label>
-        <input type="text" placeholder='Fyll inn etternavn' name='etternavn' onChange={handleInputEtternavn}/>
+        <input type="text" placeholder='Fyll inn etternavn' name='etternavn' onChange={handleInputEtternavn}  required/>
         
 
     </div>
     <div className="inputbox">
     <label><strong>Email</strong></label>
-        <input type="text" placeholder='Fyll inn email' name='email' onChange={handleInputEpost}/>
+        <input type="text" placeholder='Fyll inn email' name='email' onChange={handleInputEpost}  required/>
         
 
     </div>
     <div className="inputbox">
     <label><strong>Telefon</strong></label>
-        <input type="text" placeholder='Fyll inn telefonnummer' name='telefon' onChange={handleInputTelefon}/>
+        <input type="text" placeholder='Fyll inn telefonnummer' name='telefon' onChange={handleInputTelefon}  required/>
         
 
     </div>
     <div className="inputbox">
     <label><strong>Passord</strong></label>
-        <input type="text" placeholder='Fyll inn passord' name='passord' onChange={handleInputPassord}/>
+        <input type="text" placeholder='Fyll inn passord' name='passord' onChange={handleInputPassord}  required/>
         
 
     </div>
